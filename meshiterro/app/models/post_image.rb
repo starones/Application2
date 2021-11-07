784eda6ae019450135609a6e16f423e454f14ca3
EC2 Default User <ec2-user@ip-172-31-61-0.ec2.internal>
2021-11-07 12:28:25 +0000

    [Add]3-5

--- meshiterro/app/models/post_image.rb
+++ meshiterro/app/models/post_image.rb
@@ -5,8 +5,8 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  def favorited_by?(user)
+    def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
-  end
+    end
 
 end
